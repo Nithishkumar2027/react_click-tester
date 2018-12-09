bbd7f4e8e456549d4ca586aed8286cf657f2cdcb
Nithish kumar M.N <mnnithishkumarbtech@gmail.com>
2018-12-09 18:56:21 +0530

    Update Helper.js

--- src/Helper.js
+++ src/Helper.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 export class Helper extends React.Component {
   constructor(props) {

d803460e28d3e04921cdc5647322a9c6fb35eb81
Nithish kumar M.N <mnnithishkumarbtech@gmail.com>
2018-12-09 18:54:33 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Navbar, NavbarBrand} from 'reactstrap';
 
 export class Header extends React.Component {
   render(){
     return(
       <div>
-        <div className="container-fluid header1">
-          <h1>Click Tester</h1><span>(game)</span>
-        </div>
+        <Navbar color="dark" light expand="md">
+          <NavbarBrand><h1 id="head">Click tester <span id="span">(game)</span></h1></NavbarBrand>
+        </Navbar>
       </div>
     );
   }

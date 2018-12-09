9d3bd9c3bbee605a5317bfe6ee791631ad9a04a3
Nithish kumar M.N <mnnithishkumarbtech@gmail.com>
2018-12-09 18:58:13 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export class Footer extends React.Component {
+  render(){
+    return(
+      <div className="footer">
+      <center>
+        <span><i className="fa fa-code" id="code"></i> with <i className="fa fa-heart" id="heart" ></i> by Nithish kumar 😎</span>
+        </center>
+      </div>
+    );
+  }
+}
